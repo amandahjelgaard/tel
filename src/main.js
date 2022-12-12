91496784ece0075a827425f19db1976dc7cbbe2f
Amanda Hjelgaard <amanda.hjelgaard@hotmail.com>
2022-12-12 11:38:37 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import "./style.css";
+
+// tsParticles.load("tsparticles", {
+//   particles: {
+//     color: { value: "#fff" },
+//     move: {
+//       direction: "bottom",
+//       enable: true,
+//       outModes: "out",
+//       speed: 2,
+//     },
+//     number: {
+//       density: {
+//         enable: true,
+//         area: 2000,
+//       },
+//       value: 400,
+//     },
+//     opacity: {
+//       value: 0.7,
+//     },
+//     shape: {
+//       type: "circle",
+//     },
+//     size: {
+//       value: 4,
+//     },
+//     wobble: {
+//       enable: true,
+//       distance: 10,
+//       speed: 1,
+//     },
+//     zIndex: {
+//       value: { min: 0, max: 100 },
+//     },
+//   },
+// });
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: resolve(__dirname, "src"),
+  base: "./",
+  preview: {
+    host: true,
+  },
+  publicDir: resolve("public"),
+  build: {
+    outDir: resolve("dist"),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        kurv: resolve(__dirname, "src/kurv.html"),
+      },
+    },
+  },
+});

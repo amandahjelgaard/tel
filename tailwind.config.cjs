6192f12540eb76dbbf1155470dfc3f95929f1e2a
Amanda Hjelgaard <amanda.hjelgaard@hotmail.com>
2022-12-20 16:29:14 +0100

    comit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,49 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: { min: "640px", max: "767px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "768px", max: "1023px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "1024px", max: "1500px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: "1280px", max: "1535px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+    },
+    colors: {
+      deepgreen: "#76887D",
+      green: "#4CA154",
+      grey: "#E6E6E6",
+      purple: "#4D4775",
+      hoverknap: "#157A3B",
+      white: "#FFFFFF",
+      black: "#000000",
+      blue: "#4D4775",
+    },
+
+    fontFamily: {
+      poppins: "poppins",
+      yesevaone: "yesevaone",
+    },
+
+    fontSize: {
+      xs: ["13px"],
+      sm: ["16px"],
+      // BRØDTEKST
+      lg: ["18px", { lineHeight: "30px" }],
+      // H2
+      base: ["30px"],
+      // H1
+      xl: ["70px", { lineHeight: "70px" }],
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("daisyui")],
 };
--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        produkter: resolve(__dirname, "src/produkter.html"),
+        produkt: resolve(__dirname, "src/produkt.html"),
         kurv: resolve(__dirname, "src/kurv.html"),
+        betaling: resolve(__dirname, "src/betaling.html"),
+        betalingskort: resolve(__dirname, "src/betalingskort.html"),
+        kvittering: resolve(__dirname, "src/kvittering.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
       },
     },
   },
